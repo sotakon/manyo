ce293fcb039fabfeec28c2ab8be3480920377868
sotakon <skon1221@gmail.com>
2021-02-23 15:53:16 +0900

    taskアプリ完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -8,13 +9,41 @@ class TasksController < ApplicationController
   end
 
   def create
-    Task.create(task_params)
-    redirect_to new_task_path
+    @task = Task.new(task_params)
+    if @task.save
+    # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+    # 入力フォームを再描画します。
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました！"
   end
 
 private
-def task_params
-  params.require(:task).permit(:name, :details)
-end
+  def task_params
+    params.require(:task).permit(:name, :details)
+  end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
