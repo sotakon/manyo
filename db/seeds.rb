39f983c7aedd5fc0032245f5b0a0f52d80c612a2
sotakon <sotakon1221@gmail.com>
2021-03-03 10:50:16 +0900

    seedの管理者権限削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "管理者", email: "kanri@gmail.com", password: '123456', password_confirmation: '123456',
-             admin: true )
+User.create!(name: "管理者", email: "kanri@gmail.com", password: '123456', password_confirmation: '123456' )
